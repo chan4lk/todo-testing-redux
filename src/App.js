b216807686df1df5858f8b4100fab72d1275ef3f
Chandima Ranaweera <chan4lk@gmail.com>
2020-05-15 23:10:22 +0530

    Fixed colors

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
             <h1 className="text-grey-darkest">Todo List</h1>
             <div className="flex mt-4">
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-800"
                 placeholder="Add Todo"
               />
-              <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-black hover:bg-red">
+              <button className="flex-no-shrink p-2 border-2 rounded text-teal-500 border-teal-500 hover:text-teal-800 hover:bg-teal-500">
                 Add
               </button>
             </div>
@@ -23,21 +23,10 @@ function App() {
               <p className="w-full text-grey-darkest">
                 Add another component to Tailwind Components
               </p>
-              <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">
+              <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-green-800 text-green-500 border-green-500 hover:bg-green-500">
                 Done
               </button>
-              <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">
-                Remove
-              </button>
-            </div>
-            <div className="flex mb-4 items-center">
-              <p className="w-full line-through text-green">
-                Submit Todo App Component to Tailwind Components
-              </p>
-              <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">
-                Not Done
-              </button>
-              <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">
+              <button className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-red-800 hover:bg-red-500">
                 Remove
               </button>
             </div>
