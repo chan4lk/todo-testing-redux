7e49fbd96ef0eec7863aff3ca6083c8a122b2261
Chandima Ranaweera <chan4lk@gmail.com>
2020-05-17 06:00:55 +0530

    Updates

---
+++ precache-manifest.6a6f458177b3c652b0e1a426cfb557bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "246048757657322e649b891ddcc35bb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f58bf35de9bc3dac7bf",
+    "url": "/static/css/main.d1ec8a14.chunk.css"
+  },
+  {
+    "revision": "bd57f33bd17bc60dc98b",
+    "url": "/static/js/2.1bec114b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1bec114b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f58bf35de9bc3dac7bf",
+    "url": "/static/js/main.4bd1f3be.chunk.js"
+  },
+  {
+    "revision": "c3f6d86fb6163ec0aecb",
+    "url": "/static/js/runtime-main.57636c08.js"
+  }
+]);
