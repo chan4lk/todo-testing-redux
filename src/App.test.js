b1af61949a31e813a790824977115d5f0f2de9cc
Chandima Ranaweera <chan4lk@gmail.com>
2020-05-17 06:16:03 +0530

    add deployment to https://chan4lk.github.io/todo-testing-redux

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { render, fireEvent } from './test-utils';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render app title', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Todo List/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = getByText(/Todo List/i);
+  expect(title).toBeInTheDocument();
 });
 
 test('should add item', () => {
